274c867fed43584b51086922faab7f47f2572b71
Tinotenda Jecha <tinotendajecha@gmail.com>
2025-01-28 17:15:39 +0200

    Fixed env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
         </div>
       </nav>
 
-      {/* Hero Section */}
+      {/* Hero Sectionn */}
       <section className="pt-32 pb-20 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           <div className="grid lg:grid-cols-2 gap-12 items-center">
